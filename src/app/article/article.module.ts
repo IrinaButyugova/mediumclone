3e746d4c29e72e6785aa845a827f82a46a6c2c82
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-12-07 17:51:57 +0300

    Likes/dislakes from user post

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -14,6 +14,7 @@ import {TagListModule} from 'src/app/shared/modules/tagList/tagList.module'
 import {DeleteArticleEffect} from './store/effects/deleteArticle.effect'
 import {ArticleService} from 'src/app/article/services/article.service'
 import {FollowModule} from '../shared/modules/follow/follow.module'
+import {AddToFavoriteModule} from '../shared/modules/addToFavorites/addToFavorites.module'
 
 const routes = [
   {
@@ -31,7 +32,8 @@ const routes = [
     LoadingModule,
     ErrorMessageModule,
     TagListModule,
-    FollowModule
+    FollowModule,
+    AddToFavoriteModule
   ],
   declarations: [ArticleComponent],
   providers: [ArticleService, SharedArticleService]
